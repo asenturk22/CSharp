5aa6b61438818dd90ef8c6d9e1a888d1c1fd9f07
Abdullah Şentürk <asenturk22@yandex.com>
2024-02-05 00:48:04 +0300

    Intro eklendi

---
+++ CSharp_kamp_2024/_01_Intro/Business/CourseManager.cs
@@ -0,0 +1,44 @@
+﻿using _01_Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Intro.Business
+{
+    public class CourseManager
+    {
+        Course[] courses = new Course[3];
+        public CourseManager()
+        {
+            //Counstructor
+            Course course4 = new Course();
+            course4.Id = 1;
+            course4.Name = "C# Programlama";
+            course4.Description = "C# Programlama kursu..";
+            course4.Price = 10;
+
+            Course course5 = new Course();
+            course5.Id = 2;
+            course5.Name = "Pyton Programlama";
+            course5.Description = "Python Programlama kursu..";
+            course5.Price = 20;
+
+            Course course6 = new Course();
+            course6.Id = 3;
+            course6.Name = "Java Programlama";
+            course6.Description = "Java Programlama kursu..";
+            course6.Price = 30;
+
+            courses[0] = course4;
+            courses[1] = course5;
+            courses[2] = course6;            
+        }
+        public Course[] GetAll()
+        {
+            //Veriler veri kaynağından getirilir. 
+            return courses;
+        }
+    }
+}
---
+++ CSharp_kamp_2024/_01_Intro/Program.cs
@@ -0,0 +1,51 @@
+﻿using _01_Intro.Business;
+using _01_Intro.Entities;
+
+namespace _01_Intro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //variables --> camelCase
+            string message = "Krediler";
+            int term = 12;
+            double amount = 1000;
+            bool isAuthenticated = false;
+
+            Course course1 = new Course();
+            course1.Id = 1;
+            course1.Name = "C# Programlama";
+            course1.Description = "C# Programlama kursu..";
+            course1.Price = 10;
+
+            Course course2 = new Course();
+            course2.Id = 2;
+            course2.Name = "Pyton Programlama";
+            course2.Description = "Python Programlama kursu..";
+            course2.Price = 20;
+
+            Course course3 = new Course();
+            course3.Id = 3;
+            course3.Name = "Java Programlama";
+            course3.Description = "Java Programlama kursu..";
+            course3.Price = 30;
+
+            Course[] courses = { course1, course2, course3 };
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+            }
+
+            CourseManager coursesManager = new CourseManager();
+            Course[] courses2 = coursesManager.GetAll();
+            for (int i = 0; i < courses2.Length; i++)
+            {
+                Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+            }
+
+        }
+    }
+}
